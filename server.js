379fac1f74c3cd7e40040ef51b3d2c241cf36334
Tunde-Fadex Oluwatumininu <FA7@FADEX-PC.(none)>
2025-02-03 01:49:01 +0100

    Using Async Hash by bcrypt

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log("this is bcrypt hash:", hash);
 
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log("Res here;", res);
+    
+    });
+});
 
 
 //END_ASYNC
